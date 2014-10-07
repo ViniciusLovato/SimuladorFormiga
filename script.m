dbf5ab00706966f3be73677991a5e519b3c94504
Matheus <matheus.compri@gmail.com>
2014-10-06 23:24:20 -0300

    Melhorias no plot dos histogramas

--- script.m
+++ script.m
@@ -1,13 +1,42 @@
-tournament2
-tournament4
-tournament6
-tournament8
+% Loading the data from the files
+tournament2;
+tournament4;
+tournament6;
+tournament8;
 
-subplot(2,2,1)
-hist(x2)
-subplot(2,2,2)
-hist(x4)
-subplot(2,2,3)
-hist(x6)
-subplot(2,2,4)
-hist(x8)
+% Finding the max value between the matrices
+maxValue = max([x2(:);x4(:);x6(:);x8(:)]);
+
+% Defining the bins for the histograms
+bins = linspace(0, maxValue, maxValue + 1);
+
+% Creating the histograms for each sample
+a = hist(x2, bins);
+b = hist(x4, bins);
+c = hist(x6, bins);
+d = hist(x8, bins);
+
+% Plotting a bar graph with all the bins together
+bar(bins, [a;b;c;d]')
+
+% Changing the Layout of the histogram
+
+% Title
+title('Histograma de Torneios');
+
+% Legend
+legend("2 samples", "4 samples", "6 samples", "8 samples");
+
+% Y label
+% ylabel('Frequency');
+ylabel('Numero de formigas');
+
+% X label
+xlabel("Score");
+
+% X axis ticks
+xAxis = gca();
+set (xAxis, "XTick", [0:1:maxValue]);
+
+% Saving the image
+print('tournament.pdf', '-dpdf');
