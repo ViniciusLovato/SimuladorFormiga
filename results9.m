cdfda19142daa88f873f2c7ff93c12ff9717e8c8
Matheus <matheus.compri@gmail.com>
2014-10-30 21:11:30 -0200

    Novo gerador de graficos para octave

---
+++ results9.m
@@ -0,0 +1,62 @@
+% importing the files
+generation0;
+generation1;
+generation2;
+generation3;
+generation4;
+generation5;
+generation6;
+generation7;
+generation8;
+generation9;
+
+cleanTournament1;
+cleanTournament2;
+cleanTournament3;
+cleanTournament4;
+cleanTournament5;
+cleanTournament6;
+cleanTournament7;
+cleanTournament8;
+cleanTournament9;
+
+% plotting
+plotHistogram(g0, 1);
+print("generation0.png", "-dpng");
+plotHistogram(g1, 1);
+print("generation1.png", "-dpng");
+plotHistogram(g2, 1);
+print("generation2.png", "-dpng");
+plotHistogram(g3, 1);
+print("generation3.png", "-dpng");
+plotHistogram(g4, 1);
+print("generation4.png", "-dpng");
+plotHistogram(g5, 1);
+print("generation5.png", "-dpng");
+plotHistogram(g6, 1);
+print("generation6.png", "-dpng");
+plotHistogram(g7, 1);
+print("generation7.png", "-dpng");
+plotHistogram(g8, 1);
+print("generation8.png", "-dpng");
+plotHistogram(g9, 1);
+print("generation9.png", "-dpng");
+
+plotHistogram(c1, 1);
+print("cgen1.png", "-dpng");
+plotHistogram(c2, 1);
+print("cgen2.png", "-dpng");
+plotHistogram(c3, 1);
+print("cgen3.png", "-dpng");
+plotHistogram(c4, 1);
+print("cgen4.png", "-dpng");
+plotHistogram(c5, 1);
+print("cgen5.png", "-dpng");
+plotHistogram(c6, 1);
+print("cgen6.png", "-dpng");
+plotHistogram(c7, 1);
+print("cgen7.png", "-dpng");
+plotHistogram(c8, 1);
+print("cgen8.png", "-dpng");
+plotHistogram(c9, 1);
+print("cgen9.png", "-dpng");
